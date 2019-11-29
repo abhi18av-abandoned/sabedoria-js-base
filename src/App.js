8fd01a9e73e46722f793a571415b90480364ada2
Abhinav Sharma <abhi18av@gmail.com>
2019-11-29 23:47:44 +0530

    fetch the basic bible info

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from "axios";
+import { AUTH_TOKEN } from "./secrets";
 
 //=============================
 // constants
@@ -20,6 +22,13 @@ import React from "react";
 // services
 //=============================
 
+const axiosGet = axios.create({
+  baseURL: "https://api.scripture.api.bible/v1",
+  headers: {
+    "api-key": AUTH_TOKEN
+  }
+});
+
 //=============================
 // components
 //=============================
@@ -32,12 +41,34 @@ import React from "react";
 // App
 //=============================
 
-export const App = () => {
-  return (
-    <>
-      <div className="App">
-        <header className="App-header">Master React</header>
-      </div>
-    </>
-  );
-};
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bibleData: null
+    };
+  }
+
+  getPortugueseBibleInfo = async () => {
+    const res = await axiosGet("/bibles/90799bb5b996fddc-01");
+    this.setState({
+      bibleData: res.data.data
+    });
+  };
+
+  componentDidMount() {
+    this.getPortugueseBibleInfo();
+  }
+
+  render() {
+    return (
+      <>
+        <div className="App">
+          <header className="App-header">Master React</header>
+          <div>{JSON.stringify(this.state.bibleData)}</div>
+        </div>
+      </>
+    );
+  }
+}

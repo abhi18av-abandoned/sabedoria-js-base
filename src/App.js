76dcb6b62d8f4ba75c3791ffa5d87559d749cc8b
Abhinav Sharma <abhi18av@gmail.com>
2019-11-30 01:20:23 +0530

    made a ton of progress once I could get the right data

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const PORTUGUESE_BOOK_ID = "LUK";
 const PORTUGUESE_CHAPTER_ID = "LUK.5";
 const PORTUGUESE_PASSAGE_ID = "LUK.5.1";
 const PORTUGUESE_SECTION_ID = "LUK.S9";
+const PORTUGUESE_VERSE_ID = "LUK.5.3";
 
 //=============================
 // utils
@@ -121,30 +122,45 @@ export class App extends React.Component {
     super(props);
 
     this.state = {
-      // bibleData: null
-      allVersesData: null
+      // bibleData: null,
+      allVersesData: null,
+      aVerseData: null
     };
   }
 
-  // getAVerseInfo = async () => {
-  //   const res = await axiosGet(`/bibles/${PORTUGUESE_BIBLE_ID}`);
-  //   this.setState({
-  //     verseData: res.data.data
-  //   });
-  // };
+  getAVerseInfo = async () => {
+    //api.scripture.api.bible/v1/bibles/90799bb5b996fddc-01/verses/LUK.5.2
+    const res = await axiosGet(
+      `/bibles/${PORTUGUESE_BIBLE_ID}/verses/${PORTUGUESE_VERSE_ID}`
+    );
+    this.setState({
+      aVerseData: res.data.data
+    });
+
+    console.log(this.state.aVerseData.content);
+
+    // NOTE HTML parsing solutions for extrating or transforming fetched HTML into material design css
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(
+      this.state.aVerseData.content,
+      "text/html"
+    );
+    console.log(doc.body);
+  };
 
   getAllVersesInfo = async () => {
     // https://api.scripture.api.bible/v1/bibles/90799bb5b996fddc-01/chapters/LUK.5/verses
-    const res = await axiosGet(
+    const res = axiosGet(
       `/bibles/${PORTUGUESE_BIBLE_ID}/chapters/${PORTUGUESE_CHAPTER_ID}/verses`
     );
-    this.setState({
-      allVersesData: res.data.data
+    await this.setState(() => {
+      return { allVersesData: res.data.data };
     });
   };
 
   componentDidMount() {
-    this.getAllVersesInfo();
+    // this.getAVerseInfo();
+    // this.getAllVersesInfo();
     // this.getAllBibleInfo();
   }
 
@@ -152,8 +168,19 @@ export class App extends React.Component {
     return (
       <>
         <div className="App">
-          <header className="App-header">Master React</header>
-          <div>{JSON.stringify(this.state.allVersesData)}</div>
+          <button onClick={() => this.getAVerseInfo()}>Verse of the day</button>
+          {this.state.aVerseData ? (
+            <>
+              {/* <div>{JSON.stringify(this.state.aVerseData.id)}</div> */}
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: this.state.aVerseData.content
+                }}
+              ></div>
+            </>
+          ) : null}
+
+          {/* <div>{JSON.stringify(this.state.allVersesData)}</div> */}
         </div>
       </>
     );

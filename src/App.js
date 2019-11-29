897af615749072cb82f89f9417fe201a2f852b46
Abhinav Sharma <abhi18av@gmail.com>
2019-11-30 00:18:03 +0530

    add the various endpoints and string constants

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import { AUTH_TOKEN } from "./secrets";
 // constants
 //=============================
 
+const PORTUGUESE_BIBLE_ID = "90799bb5b996fddc-01";
+const PORTUGUESE_BOOK_ID = "LUK";
+const PORTUGUESE_CHAPTER_ID = "LUK.5";
+const PORTUGUESE_PASSAGE_ID = "LUK.5.1";
+const PORTUGUESE_SECTION_ID = "LUK.S9";
+
 //=============================
 // utils
 //=============================
@@ -29,6 +35,75 @@ const axiosGet = axios.create({
   }
 });
 
+// getABibleInfo = async () => {
+// https://api.scripture.api.bible/v1/bibles/90799bb5b996fddc-01
+//   const res = await axiosGet(`/bibles/${PORTUGUESE_BIBLE_ID}`);
+//   this.setState({
+//     bibleData: res.data.data
+//   });
+// };
+
+// getAllBibleInfo = async () => {
+//  https://api.scripture.api.bible/v1/bibles
+//   const res = await axiosGet(`/bibles`);
+//   this.setState({
+//     bibleData: res.data.data
+//   });
+// };
+
+// getABookInfo = async () => {
+//   const res = await axiosGet(`/bibles/${PORTUGUESE_BIBLE_ID}`);
+//   this.setState({
+//     bibleData: res.data.data
+//   });
+// };
+
+// getAllBooksInfo = async () => {
+// https://api.scripture.api.bible/v1/bibles/90799bb5b996fddc-01/books
+//   const res = await axiosGet(`/bibles`);
+//   this.setState({
+//     bibleData: res.data.data
+//   });
+// };
+
+// getAChapterInfo = async () => {
+//   const res = await axiosGet(`/bibles/${PORTUGUESE_BIBLE_ID}`);
+//   this.setState({
+//     bibleData: res.data.data
+//   });
+// };
+
+// getAllChaptersInfo = async () => {
+// https://api.scripture.api.bible/v1/bibles/90799bb5b996fddc-01/books/LUK/chapters
+//   const res = await axiosGet(`/bibles`);
+//   this.setState({
+//     bibleData: res.data.data
+//   });
+// };
+
+// getAPassageInfo = async () => {
+// https://api.scripture.api.bible/v1/bibles/90799bb5b996fddc-01/passages/LUK.5.1
+//   const res = await axiosGet(`/bibles/${PORTUGUESE_BIBLE_ID}`);
+//   this.setState({
+//     bibleData: res.data.data
+//   });
+// };
+
+// getASectionInfo = async () => {
+//   const res = await axiosGet(`/bibles/${PORTUGUESE_BIBLE_ID}`);
+//   this.setState({
+//     bibleData: res.data.data
+//   });
+// };
+
+// getAllSectionsInfo = async () => {
+// https://api.scripture.api.bible/v1/bibles/90799bb5b996fddc-01/books/LUK/sections
+//   const res = await axiosGet(`/bibles`);
+//   this.setState({
+//     bibleData: res.data.data
+//   });
+// };
+
 //=============================
 // components
 //=============================
@@ -46,19 +121,31 @@ export class App extends React.Component {
     super(props);
 
     this.state = {
-      bibleData: null
+      // bibleData: null
+      allVersesData: null
     };
   }
 
-  getPortugueseBibleInfo = async () => {
-    const res = await axiosGet("/bibles/90799bb5b996fddc-01");
+  // getAVerseInfo = async () => {
+  //   const res = await axiosGet(`/bibles/${PORTUGUESE_BIBLE_ID}`);
+  //   this.setState({
+  //     verseData: res.data.data
+  //   });
+  // };
+
+  getAllVersesInfo = async () => {
+    // https://api.scripture.api.bible/v1/bibles/90799bb5b996fddc-01/chapters/LUK.5/verses
+    const res = await axiosGet(
+      `/bibles/${PORTUGUESE_BIBLE_ID}/chapters/${PORTUGUESE_CHAPTER_ID}/verses`
+    );
     this.setState({
-      bibleData: res.data.data
+      allVersesData: res.data.data
     });
   };
 
   componentDidMount() {
-    this.getPortugueseBibleInfo();
+    this.getAllVersesInfo();
+    // this.getAllBibleInfo();
   }
 
   render() {
@@ -66,7 +153,7 @@ export class App extends React.Component {
       <>
         <div className="App">
           <header className="App-header">Master React</header>
-          <div>{JSON.stringify(this.state.bibleData)}</div>
+          <div>{JSON.stringify(this.state.allVersesData)}</div>
         </div>
       </>
     );
